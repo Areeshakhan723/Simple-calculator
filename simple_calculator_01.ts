7964dbd5cc3a5fdd106489570de23b85195c58de
Areesha  Khan <160509293+Areeshakhan723@users.noreply.github.com>
2024-03-30 20:49:06 +0500

    Add files via upload

---
+++ simple_calculator_01.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([                 // save
+  { message: "Enter a first number", type: "number", name: "firstNumber" },
+  { message: "Enter a second number", type: "number", name: "secondNumber" },
+  {
+    message: "select one of the operators to perform some specific operation",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+  },
+]);
+
+console.log(answer);
+
+if(answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if(answer.operator === "Subtraction"){
+    console.log(answer.firstNumber - answer.secondNumber);
+}else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.secondNumber);
+}else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.secondNumber);
+}else {
+    console.log("Please select a valid operator")
+};
